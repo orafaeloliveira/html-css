befe4a8562d69699867fa203452ea8bdec31fc8a
Rafael Oliveira <51241822+orafaeloliveira@users.noreply.github.com>
2022-10-06 13:30:15 -0300

    add media-query.css

--- exercícios/ex026/mq005/scripts/script.js
+++ exercícios/ex026/mq005/scripts/script.js
@@ -1,8 +1,15 @@
 function abrirMenu(){
     if (menu.style.display == "block") {
-        menu.style.display = "none";
+        menu.style.display = "none"
     } else {
         menu.style.display = "block"
     }
+}
 
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        menu.style.display = "block"
+    } else {
+        menu.style.display = "none"
+    }
 }
